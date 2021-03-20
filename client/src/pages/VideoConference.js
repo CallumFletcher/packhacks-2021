d8a80593569b7c7246dca2aa3ad4194aed781308
Chinemerem Chigbo <chinemeremchigbo@Outlook.com>
2021-03-20 14:31:52 -0400

    Attempt to make jitsi work

--- client/src/pages/VideoConference.js
+++ client/src/pages/VideoConference.js
@@ -1,24 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import Jitsi from "react-jitsi";
 
 function VideoConference(props) {
-  const email = "cool@gmail.com";
-  const name = "Cool Cool";
-  const roomName = email + name;
+  const roomName =
+    "my-super-secret-meeting-123e4567-e89b-12d3-a456-426655440000";
+  const name = "Joseph Strawberry";
 
   const handleAPI = (JitsiMeetAPI) => {
     console.log(JitsiMeetAPI);
   };
 
   return (
-    <React.Fragment>
-      <Jitsi
-      //onAPILoad={handleAPI}
-      //roomName={roomName}
-      //displayName={name}
-      //config={{ prejoinedPageEnabled: false }}
-      />
-    </React.Fragment>
+    <Jitsi
+      onAPILoad={handleAPI}
+      roomName={roomName}
+      displayName={name}
+      config={{ prejoinPageEnabled: false }}
+    />
   );
 }
 
