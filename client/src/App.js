afccac7d8052286e9c8071164647c14dd1c9b39f
Callum Fletcher <callumafletcher@gmail.com>
2021-03-20 09:33:35 -0700

    landing page + lobby skeleton, basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,79 +1,21 @@
-import React, { useState, useEffect } from "react";
-import io from "socket.io-client";
-import { TextField, Button } from "@material-ui/core";
+import React from "react";
 
-//for local testing, change to localhost:5000
-//the react proxy doesn't like socket connections
-const socket = io.connect("localhost:5000");
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import Lobby from "./pages/Lobby";
 
 function App(props) {
-  const [message, setMessage] = useState({ message: "", name: "" });
-  const [chat, setChat] = useState([]);
-  //lkjlkj
-  useEffect(() => {
-    socket.on("message", (message) => {
-      setChat((prev) => [...prev, message]);
-      console.log(message);
-    });
-  }, []);
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100%",
-      }}
-    >
-      <TextField
-        style={{ paddingBottom: 10 }}
-        variant="outlined"
-        placeholder="name"
-        value={message.name}
-        onChange={(e) => {
-          setMessage((prev) => ({ ...prev, name: e.target.value }));
-        }}
-      />
-      <div
-        style={{
-          height: 700,
-          width: 400,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "flex-start",
-          overflow: "scroll",
-          border: "solid",
-        }}
-      >
-        {chat.map((message) => (
-          <div style={{ margin: 0, padding: 0 }}>
-            <span>
-              <p>{message.message}</p> <p>-{message.name}</p>
-            </span>
-          </div>
-        ))}
-      </div>
-      <TextField
-        placeholder="message"
-        style={{ width: 300 }}
-        variant="outlined"
-        value={message.message}
-        onChange={(e) => {
-          setMessage((prev) => ({ ...prev, message: e.target.value }));
-        }}
-      />
-      <Button
-        variant="contained"
-        onClick={(e) => {
-          e.preventDefault();
-          socket.emit("message", message);
-          setMessage((prev) => ({ ...prev, message: "" }));
-        }}
-      >
-        Submit
-      </Button>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <LandingPage />
+        </Route>
+        <Route exact path="/lobby">
+          <Lobby />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,2 +1,22 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import { Typography, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+const LandingPage = (props) => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Typography variant="h1">Welcome My Dudes</Typography>
+    </div>
+  );
+};
+
+export default LandingPage;
