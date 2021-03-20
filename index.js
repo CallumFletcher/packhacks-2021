7327fb457cc220783ed3187dce3ab28643151fd7
Callum Fletcher <callumafletcher@gmail.com>
2021-03-20 08:14:30 -0700

    react setup

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const path = require("path");
+const http = require("http");
+const socketIo = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, { cors: { origin: "*" } });
 
-app.get("/", ({ req, res }) => {
-  res.send("hello");
+io.on("connection", (socket) => {
+  console.log("new connection");
+});
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-app.listen(process.env.PORT || 5000, () => {
+server.listen(process.env.PORT || 5000, () => {
   console.log("listening on port " + 5000);
 });
