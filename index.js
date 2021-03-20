788d5787f41e68a2fa3583e387e81bf5b9a84fb5
Callum Fletcher <callumafletcher@gmail.com>
2021-03-20 08:09:30 -0700

    express set up

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", ({ req, res }) => {
+  res.send("hello");
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("listening on port " + 5000);
+});
