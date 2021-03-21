cc6a823f906a6f2333ca4a608727daa6d398e1ba
DanielOliyarnik <81031004+DanielOliyarnik@users.noreply.github.com>
2021-03-21 09:41:44 -0400

    new add

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,29 +1,88 @@
 import React from "react";
 import "./Contact.css"
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { useHistory } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 
 const Contact = (props) => {
+  const history = useHistory();
   return (
     <section className="contact">
+      
+      <Link to="/">
+      <div className="back-img"></div>
+      </Link>
+     
         <div className="contact-wrapper">
-            <h1 className="contact-heading">Contact</h1>
-            <form>
-              <div className="input-group">
-                <input type="text" className="field" />
-                <label className="input-label">Full Name</label>
-              </div>
-              <div className="input-group">
-                <input type="email" className="field" />
-                <label className="input-label">Email</label>
-              </div>
-              <div className="input-group">
-                <textarea className="field"></textarea>
-                <label className="message">Message</label>
-              </div>
-              <input type="submit" className="submit-btn" value="Submit" />
-            </form>
+
+        <TextField
+          id="filled-name-input"
+          label=" Name"
+          type="name"
+          variant="filled"
+          color="primary"
+          style = {{width: 300, background: "lightblue"}}
+          InputLabelProps={{
+            style: {
+              textOverflow: 'ellipsis',
+              whiteSpace: 'nowrap',
+              overflow: 'hidden',
+              width: '100%',
+              color: 'black',
+              opacity: 1,
+            }
+          }}
+        />
+        <TextField
+          id="filled-name-input"
+          label=" Email"
+          type="email"
+          variant="filled"
+          color="primary"
+          style = {{width: 300, background: "lightblue"}}
+          InputLabelProps={{
+            style: {
+              textOverflow: 'ellipsis',
+              whiteSpace: 'nowrap',
+              overflow: 'hidden',
+              width: '100%',
+              color: 'black'
+            }
+          }}
+        />
+        <TextField
+          id="filled-name-input"
+          label=" Text"
+          type="text"
+          variant="filled"
+          color="primary"
+          style = {{width: 300, background: "lightblue"}}
+          InputLabelProps={{
+            style: {
+              textOverflow: 'ellipsis',
+              whiteSpace: 'nowrap',
+              overflow: 'hidden',
+              width: '100%',
+              color: 'black',
+              fontsize: 5
+            }
+          }}
+        />
+        <br/><br/>
+        <Button 
+        variant="outlined" 
+        color="primary"
+        >
+          Send Email
+        </Button>
+
         </div>
       </section>
+
+
+
   );
 };
 
